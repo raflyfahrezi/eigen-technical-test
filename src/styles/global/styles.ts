f0ff1d07c378ad04449aed3dc4fa3b664351ec6a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-17 21:29:05 +0700

    create global styles

---
+++ src/styles/global/styles.ts
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react'
+
+export const sGlobalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+
+    box-sizing: border-box;
+  }
+
+  p {
+    margin: 0 !important;
+  }
+`
