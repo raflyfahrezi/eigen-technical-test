09397b62a37e732611bc9527fafdffb6c75a4e1d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-17 21:29:56 +0700

    create pageWrapper layout

---
+++ src/layout/pageWrapper/styles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/css'
+
+export const sPageWrapper = css`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 1fr auto;
+`
---
+++ src/layout/pageWrapper/types.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react'
+
+export type TPageWrapperProps = {
+  title: string
+  children: ReactNode
+}
