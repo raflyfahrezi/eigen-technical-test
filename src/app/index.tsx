43db3306985887635bbd9336732fa9e47d639629
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-17 21:30:36 +0700

    adding routes on app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './helpers'
 
 const App = () => {
-  return <div>App</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
