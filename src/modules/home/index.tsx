8e62dc91f68ba3abd41911c4744fb897096149f8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-17 21:30:24 +0700

    create home modules

---
+++ src/modules/home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Wrapper } from '@/components'
+import { PageWrapper } from '@/layout'
+import { Paragraph } from '@/typography'
+
+const HomeModules = () => {
+  return (
+    <PageWrapper title='Home'>
+      <div>
+        <Wrapper>
+          <Paragraph>Testing</Paragraph>
+        </Wrapper>
+      </div>
+    </PageWrapper>
+  )
+}
+
+export default HomeModules
