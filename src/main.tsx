305a6dbbd54a764af3d21e3e38982579f819b6fc
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-17 21:30:43 +0700

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 
 import App from '@/app'
+import { Global } from '@/styles'
 
-import './index.css'
+import 'antd/dist/antd.css'
+import '@fontsource/open-sans/400.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Global />
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 )
