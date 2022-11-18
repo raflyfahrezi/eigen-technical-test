3b41261928aa31167f83550660437db09c7685c4
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-18 15:33:59 +0700

    fix jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const jestConfig = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default jestConfig
