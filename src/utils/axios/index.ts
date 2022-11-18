6f203e21613fc3581f17930a207bc77a0a99a3e3
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-11-18 15:34:22 +0700

    fix axios

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+import { VITE_BASE_URL } from '@/constant'
+
 import { AxiosProps } from './types'
 import { getFormattedURLWithAPIKey } from './helpers'
 
 export const axiosGet = <T>(url: string, config?: AxiosProps) => {
   return axios.get<T>(getFormattedURLWithAPIKey(url), {
     ...config,
-    baseURL: config?.withoutBaseURL ? '' : import.meta.env.VITE_BASE_URL,
+    baseURL: config?.withoutBaseURL ? '' : VITE_BASE_URL,
   })
 }
